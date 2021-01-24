1021478c43d118e6b0f80c098880dce009a91bca
ADRIAN MENESES <itoyan@meya.local>
2021-01-24 11:43:45 -0500

    Updated to 32 bits

--- C/getBinary.c
+++ C/getBinary.c
@@ -1,21 +1,24 @@
+/*
+ * Program will return the binary representation of a (positive) number.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-/* Program to return the binary representation of a number. */
 int main() {
 	
 	char * binary;
 	char buffer[33];
 	int number = 0;
 
-	printf("Enter a number to get its binary representation\n");
+	printf("Enter a number to get its binary representation: ");
 	fgets(buffer, 32, stdin);
 
 	printf("\nThe number entered was: %s\n", buffer);
 	
 	number  = atoi(buffer);
-	binary = malloc(sizeof(int) + 1);
+	binary = malloc(sizeof(int));
 	if(binary == NULL) {
 		printf("Memory allocation failed! Try again!\n");
 	}
@@ -23,17 +26,13 @@ int main() {
 	int stop = 0;
 	int mask = 0;
 	int counter = 0;
-	int i = sizeof(binary) - 1;
-	i = 39;
-	binary[40] = '\0';
-	printf("size of binary is = %d\n", i);
-	printf("i = %d\n", i);
+	int i = 32; // To save the bits from MSB to LSB
+	binary[32] = '\0';
 
 	while(stop != 1) {
 		mask = number & 0x01;
 		number >>= 0x01;
 
-	//	if((counter % 4) == 0) { binary[i - 1] = ' '; } 
 		if(mask == 1) { binary[i] = '1'; }
 		else { binary[i] = '0'; }
 
