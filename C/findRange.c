8ededff696983ea20279883cebf8f0515b712741
ADRIAN MENESES <adgeme@hotmail.com>
2021-01-30 19:30:57 -0500

    Initial version

---
+++ C/findRange.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+
+	float n = 10.01;
+	int found = 0;
+	int counter = 0;
+
+	if(n > 0.0 && n < 1) {
+		puts("it is within 0 and 1");
+	}
+	else if(n >=1 && n < 2) {
+		puts("n is within 1 and 2");
+	}
+	else {
+		puts("n is negative");
+	}
+
+
+	n = 52.0;
+	while(found != 1) {
+		if((n >= 1) && (n < 2)) {
+			printf("n = %.4f and E = %d\n", n, counter);
+			found = 1;
+		}
+		else {
+			n /= 2;
+			printf("n = %.4f\n", n);
+			counter++;
+		}
+	}
+	
+	int x = 9;
+	int i = 0;
+	printf("x = %d\n", x);
+	for(i = 10; i >= 0; i--) {
+		if(i & x) {
+			printf("1");
+		}
+		else {
+			printf("0");
+		}
+	}
+	printf("\n");
+
+	return 0;
+}
