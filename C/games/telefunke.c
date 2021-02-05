7f2f292d376b3884870ebc582166bf14bde1c0fa
ADRIAN MENESES <adgeme@hotmail.com>
2021-02-05 14:44:31 -0500

    Added comments

--- C/games/telefunke.c
+++ C/games/telefunke.c
@@ -201,18 +201,18 @@ void printDividers() {
  * 		with respect to whoever is currently leading the 
  * 		scoreboard.
  */
-void scoreDiff(char players[][MAX_NAME]/*, int *totalScores*/) {
+void scoreDiff(char players[][MAX_NAME]) {
 
 	int i = 0;
 	int lowestScore[1];
 	int highestScore[1];
 	int indexLow = 0;
 	int indexHigh = 0;
-	//char positions[6];
 
 	lowestScore[0] = totalScore[0][0]; // Start comparing from index 0
 	highestScore[0] = totalScore[0][0];
-	//printf("lowest %d\n", lowestScore[0]);
+
+	/* Get the lowest and highest score as well as index */
 	for(i = 0; i < SIX; i++) {
 		if(totalScore[i][0] == - 1) { continue; }
 		if(totalScore[i][0] < lowestScore[0] && totalScore[i][0] > 0) {
@@ -225,18 +225,14 @@ void scoreDiff(char players[][MAX_NAME]/*, int *totalScores*/) {
 		}
 	}
 
-	//printf("lowest after %d\n", lowestScore[0]);
 	/* Print the difference */
 	for(i = 0; i < SIX; i++) {
-		if(totalScore[i][0] < 0) {
-			continue;
-		}
+		if(totalScore[i][0] < 0) { continue; }
 		printf("%d) %-7s +%d", (i+1), players[i], totalScore[i][0] - lowestScore[0]);
 		if(i == indexHigh) {
 			printf("☠ ");
 		}
 		puts("");
-		//printf("total[%d] = %d\n",i, *(totalScores[i]));
 	}
 }
 
@@ -455,7 +451,7 @@ void gameWith5() {
 			enterScores(players, totalScores, game);
 			saveScores(players, totalScores, game);	
 			printTable();
-			scoreDiff(players/*,totalScores*/);
+			scoreDiff(players);
 			puts("\n");
 		}
 		round++;
